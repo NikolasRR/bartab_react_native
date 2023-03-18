4eceef996dda66c87e1abea2735a8705c73fe769
Nikolas <nkls99@gmail.com>
2023-03-18 13:15:02 -0300

    home screen styling

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,21 @@
-import { useEffect } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>nelson</Text>
-      <Button title='go' onPress={() => navigation.navigate("AddParticipants")}></Button>
+      <View style={styles.textBox}>
+        <Text style={textStyles(29, 'Montserrat').welcome}>Welcome to Bartab!</Text>
+        <Text style={textStyles(17, 'Montserrat-medium').welcome}>the app to split the bar tab with your friends</Text>
+      </View>
+      <View style={styles.textBox}>
+        <Text style={textStyles(20, 'Montserrat').welcome}>Here's how it works:</Text>
+        <Text style={textStyles(16, 'Montserrat-medium').welcome}>first, add the participants</Text>
+        <Text style={textStyles(16, 'Montserrat-medium').welcome}>then, add the items, informing the ones that participated in it</Text>
+      <Text style={textStyles(16, 'Montserrat-medium').welcome}>and that's it! the indivual check of every participant will be calculated, as well as the total</Text>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("AddParticipants")}>
+        <Text style={styles.buttonText}>start</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,6 +25,34 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#EEE8AA',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  textBox: {
+    marginTop: 30
+  },
+  button: {
+    backgroundColor: '#ffd700',
+    height: 60,
+    width: 250,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginTop: 60
+  },
+  buttonText: {
+    color: '#8b4513',
+    fontFamily: 'Montserrat',
+    fontSize: 25
+  }
 });
+
+const textStyles = (size, font) => StyleSheet.create({
+  welcome: {
+    color: '#D2691E',
+    width: 300,
+    textAlign: 'center',
+    fontFamily: font,
+    fontSize: size,
+    marginBottom: 10
+  },
+})
