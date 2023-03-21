90cb04e6eaab4b4c6fdd751f49a635a9f6c3aabe
Nikolas <nkls99@gmail.com>
2023-03-21 17:47:30 -0300

    TotalTab style change

--- src/components/TabDisplay/TotalTab/styles.js
+++ src/components/TabDisplay/TotalTab/styles.js
@@ -6,13 +6,13 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: 10,
-    height: 50,
+    height: 60,
     backgroundColor: '#D2691E'
   },
   text: {
     fontSize: 25,
     fontWeight: '700',
-    color: '#4A0404'
+    color: '#ffd700'
   }
 })
 
