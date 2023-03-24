26f1725192389f0c806d84c8e41cb0db1d5e39ed
Nikolas <nkls99@gmail.com>
2023-03-24 16:07:55 -0300

    added icons and splashScreen for build

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <StatusBar translucent={true}/>
+      <StatusBar translucent={true} backgroundColor={'black'}/>
       <Header />
       <Stack.Navigator>
         <Stack.Screen
