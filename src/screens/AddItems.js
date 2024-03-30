14dbfa3211387e5474cfb3a148dd1477ce5cd579
Nikolas <nkls99@gmail.com>
2024-03-30 15:46:12 -0300

    feat: Implements the items context

--- src/screens/AddItems.js
+++ src/screens/AddItems.js
@@ -1,16 +1,14 @@
-import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
 import ItemForm from "../components/AddItems/ItemForm/ItemForm";
 import ItemList from "../components/AddItems/ItemsList/ItemsList";
 
 export default function AddItems({ navigation, route }) {
-  const [items, setItems] = useState([]);
 
   return (
     <View style={styles.container}>
-      <ItemList items={items} setItems={setItems} />
-      <ItemForm participants={route.params.participants} items={items} setItems={setItems} navigation={navigation}/>
+      <ItemList />
+      <ItemForm navigation={navigation}/>
     </View>
   );
 }
