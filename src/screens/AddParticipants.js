fd119c7faa9463cb4c292a409dad34e9d59b4dad
Nikolas <nkls99@gmail.com>
2024-03-30 15:47:00 -0300

    refactor: Removes unecessary state

--- src/screens/AddParticipants.js
+++ src/screens/AddParticipants.js
@@ -1,16 +1,14 @@
-import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
 import ParticipantForm from "../components/AddParticiapants/ParticipantFrom/ParticipantForm";
 import ParticipantList from "../components/AddParticiapants/ParticipantList/ParticipantsList";
 
 export default function AddParticipants({ navigation }) {
-  const [participants, setParticipants] = useState([]);
 
   return (
     <View style={styles.container}>
-      <ParticipantList participants={participants} setParticipants={setParticipants} />
-      <ParticipantForm participants={participants} setParticipants={setParticipants} navigation={navigation} />
+      <ParticipantList />
+      <ParticipantForm navigation={navigation} />
     </View>
   );
 }
