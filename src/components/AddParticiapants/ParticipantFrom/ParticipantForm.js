bae9a86bc8b68fb635f353c958f75ecbcd84d868
Nikolas <nkls99@gmail.com>
2024-03-30 15:49:03 -0300

    feat: Implements participants context

--- src/components/AddParticiapants/ParticipantFrom/ParticipantForm.js
+++ src/components/AddParticiapants/ParticipantFrom/ParticipantForm.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
 import styles, { continueButton, continueButtonText } from "./styles";
+import { useParticipants } from '../../../contexts/participantContext';
 
-export default function ParticipantForm({ participants, setParticipants, navigation }) {
+export default function ParticipantForm({ navigation }) {
+  const { participants, setParticipants } = useParticipants();
+
   const [participantName, setParticipantName] = useState("");
 
   const enoughParticipants = participants.length <= 1;
@@ -34,7 +37,7 @@ export default function ParticipantForm({ participants, setParticipants, navigat
       <TouchableOpacity
         disabled={enoughParticipants}
         style={continueButton(enoughParticipants ? '#959595' : '#ffd700').style}
-        onPress={() => navigation.navigate("AddItems", { participants: participants })}
+        onPress={() => navigation.navigate("AddItems")}
         activeOpacity={1}
       >
         <Text style={continueButtonText(enoughParticipants ? '#d0d0d0' : '#8b4513').style}>continue</Text>
